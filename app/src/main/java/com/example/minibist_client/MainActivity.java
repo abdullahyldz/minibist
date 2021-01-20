bec53f4919db45d496beb36fbb56ff53ba67b759
abdullahyldz <abdullahyildiz321@gmail.com>
2021-01-20 22:45:14 +0300

    update remote repo

--- app/src/main/java/com/example/minibist_client/MainActivity.java
+++ app/src/main/java/com/example/minibist_client/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignupActivity.class));
-            }
+            } 
         });
         /*
         * String str = text.getText().toString();
