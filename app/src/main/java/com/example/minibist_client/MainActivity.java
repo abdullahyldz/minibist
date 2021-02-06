7f1c46f95fe333bb86df008259659253a28eeea1
abdullahyldz <abdullahyildiz321@gmail.com>
2021-02-06 14:54:38 +0300

    signup serialization-comm

--- app/src/main/java/com/example/minibist_client/MainActivity.java
+++ app/src/main/java/com/example/minibist_client/MainActivity.java
@@ -5,15 +5,10 @@ import java.net.Socket;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.minibist_client.model.Task;
-import com.example.minibist_client.serializer.LoginContent;
-import com.example.minibist_client.serializer.LoginSerializer;
-import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
 
--- app/src/main/java/com/example/minibist_client/SignupActivity.java
+++ app/src/main/java/com/example/minibist_client/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.minibist_client;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -8,6 +9,20 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.minibist_client.model.ServerResponse;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -93,10 +108,106 @@ public class SignupActivity extends AppCompatActivity {
             String password = etPassword.getText().toString();
             String repeatPassword = etRepeatPassword.getText().toString();
 
+            JSONObject content = new JSONObject();
+            JSONObject message = new JSONObject();
+            try {
+                content.put("firstName", etFirstName.getText().toString());
+                content.put("lastName", etLastName.getText().toString());
+                content.put("email", etEmail.getText().toString());
+                content.put("password", etPassword.getText().toString());
+                message.put("operation", "signup");
+                message.put("message", content.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            SignupActivity.SocketHandler handler = new SignupActivity.SocketHandler(message.toString());
+            handler.execute();
+
             Toast.makeText(this,"Signup Successful",Toast.LENGTH_SHORT).show();
             // Here you can call you API
             Intent signupSuccess = new Intent(this, LoginActivity.class);
             startActivity(signupSuccess);
+        }
+    }
+
+    public class SocketHandler extends AsyncTask<Void, Void, JSONObject> {
+        private Socket socket;
+        BufferedReader reader;
+        BufferedWriter writer;
+        private String message;
+        Gson gson;
+        private ServerResponse serverResponse = null;
+
+        public SocketHandler(String message){
+            this.message = message;
+            this.gson = new Gson();
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... voids) {
+            try {
+                InetAddress serverAddr = InetAddress.getByName("10.0.2.2");
+                socket = new Socket(serverAddr, 6666);
+                writer = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream()));
+                reader = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            JSONObject response = null;
+
+            try {
+                writer.write(this.message);
+                writer.newLine();
+                writer.flush();
+                String responseStr = reader.readLine();
+                ServerResponse serverResponse = this.gson.fromJson(responseStr, ServerResponse.class);
+                this.serverResponse = serverResponse;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        protected void onPostExecute(JSONObject req) {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (this.serverResponse.getStatus().equals("success")) {
+                Toast.makeText(SignupActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                Intent signupSuccess = new Intent(SignupActivity.this, LoginActivity.class);
+                SignupActivity.this.startActivity(signupSuccess);
+            } else {
+                Toast.makeText(SignupActivity.this, this.serverResponse.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        public ServerResponse getServerResponse() {
+            return serverResponse;
         }
     }
 
