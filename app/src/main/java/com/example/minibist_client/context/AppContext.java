32efab654b8c3a633642628b15543734da1f88b7
abdullahyldz <abdullahyildiz321@gmail.com>
2021-02-12 22:42:44 +0300

    deployed server

--- app/src/main/java/com/example/minibist_client/context/AppContext.java
+++ app/src/main/java/com/example/minibist_client/context/AppContext.java
@@ -9,7 +9,7 @@ public class AppContext {
 
     public static String email;
     public static final Integer port = 8080;
-    public static final String ip = "10.0.2.2"; // ec2 : 3.250.108.35
+    public static final String ip = "3.250.108.35"; // ec2 : 3.250.108.35
     public static Integer money = 100;
     // 10.0.2.2 InetAddress serverAddr = InetAddress.getByName("52.86.12.80"); // 52.86.12.80stormy-wave-53190.herokuapp.com
     // 52.86.12.80
